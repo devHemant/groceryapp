4b14fed966eba441e1176b0c72137515c8d59b86
Hemant <info.hemantkumar17@gmail.com>
2021-09-14 21:04:21 +0530

    added hero-contianer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="jumbotron text-center">
-      <p className="lead mt-3">Welcome to Grocery Store</p>
+    <div className="hero-contianer text-center ">
+      <div className="pt-5">
+        <p className="lead">Welcome to Grocery Store</p>
 
-      <p>Primarily engaged in retailing a general range of food products.</p>
-      <a className="btn btn-outline-light btn-md" href="#" role="button">
-        Start Shopping
-      </a>
-      <hr />
+        <p>Primarily engaged in retailing a general range of food products.</p>
+        <a className="btn btn-warning btn-md" href="#" role="button">
+          Start Shopping
+        </a>
+      </div>
     </div>
   );
 };
