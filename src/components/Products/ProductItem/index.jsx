0ae19812994b7e8751bc6524275966f9c5a2a389
Hemant <info.hemantkumar17@gmail.com>
2021-09-14 18:35:57 +0530

    fixed product iteam height

--- src/components/Products/ProductItem/index.jsx
+++ src/components/Products/ProductItem/index.jsx
@@ -3,19 +3,20 @@ import React from "react";
 const ProductItem = (props) => {
   const { productName, image, price, mrp, unit, description } = props.product;
   return (
-    <div className="col-sm-3">
+    <div className="col-sm-4 mt-2">
       <div className="card">
         <img
           src={"https://rjtmobile.com/grocery/images/" + image}
           className="card-img-top"
           alt={"image"}
+          height="200px"
         />
         <div className="card-body">
-          <h5 className="card-title">{productName}</h5>
+          <h6 className="card-title">{productName}</h6>
           <p className="card-text">{unit}</p>
           <h2>
             {price}{" "}
-            <span style={{ fontSize: "20px", color: "grey" }}>
+            <span style={{ fontSize: "16px", color: "grey" }}>
               <del> {mrp}</del>
             </span>
           </h2>
